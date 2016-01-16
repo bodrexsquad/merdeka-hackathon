7d34bc927d67d9f8e3a50d97dfae05f86d21cd37
Ivan Teong <ivanteong@hotmail.com>
2016-01-17 09:18:04 +1100

    Linking postgresql database with sqlite as heroku does not support sqlite3

--- Gemfile
+++ Gemfile
@@ -1,27 +1,35 @@
 source 'https://rubygems.org'
 ruby '2.2.1'
 gem 'rails', '4.2.1'
-gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
+
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
 end
+
 gem 'bootstrap-sass'
 gem 'high_voltage'
 gem 'omniauth'
 gem 'omniauth-facebook'
 gem 'simple_form'
+
 group :development do
   gem 'better_errors'
   gem 'hub', :require=>nil
   gem 'quiet_assets'
   gem 'rails_layout'
+  gem 'sqlite3'
+end
+
+group :production do
+	gem 'pg'
+	gem "activerecord-postgresql-adapter"
 end
 
 gem 'koala'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+configure :development do
+  set :database, "sqlite3:///database.db"
+end
+
+configure :production do
+  db = URI.parse(ENV['postgres://iqwljiwwoqdysd:qzGRGgasKi_sMysSP4dcabR8Km@ec2-54-83-61-45.compute-1.amazonaws.com:5432/dde6roet07bko1'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
